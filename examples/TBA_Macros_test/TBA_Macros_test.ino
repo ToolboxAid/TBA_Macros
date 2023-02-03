62b691a5a29e1a1bef84662b391ab597d140636e
toolboxaid <97194984+ToolboxAid@users.noreply.github.com>
2023-02-03 10:22:19 -0500

    Added boolean format macro

--- examples/TBA_Macros_test/TBA_Macros_test.ino
+++ examples/TBA_Macros_test/TBA_Macros_test.ino
@@ -7,19 +7,21 @@ void setup()
   // Use serial port
   Serial.begin(115200);
   while (!Serial && (millis() <= 1000))
-  {  };
+  {
+  };
 
   Serial.print(" <");
   Serial.print(__FILENAME__);
   Serial.print("> ");
 
   Serial.println();
+
+  Serial.println(formatBool(true));
+  Serial.println(formatBool(false));
 }
 
 void loop()
 {
   delay(1000);
-  Serial.print  (" <");
-  Serial.print  (__FILENAME__);
-  Serial.println("> ");
+  Serial.print(".");
 }
--- src/TBA_Macros.h
+++ src/TBA_Macros.h
@@ -4,3 +4,5 @@
 #define __FILENAME_FS__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : strrchr(__FILE__, '\\') + 1 )
+
+#define formatBool(b) ((b) ? "true" : "false")
